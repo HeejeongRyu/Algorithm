42904829dfb30e5830e69b4ba2d6fcc3ee7ec0bb
HeejeongRyu <95456557+HeejeongRyu@users.noreply.github.com>
2022-01-28 14:56:02 +0900

    2022-01-27
    2022-01-27

---
+++ src/Bronze/Bronze_1_11050.java
@@ -0,0 +1,31 @@
+package Algorithm_2022_01_27;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Bronze_1_11050 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		System.out.println(BC(N, K));
+
+	}
+
+	static int BC(int n, int k) {
+
+		if(n == k || k == 0) {
+			return 1;
+		}
+
+		return BC(n - 1, k - 1) + BC(n - 1, k);
+	}
+}
