caf9d03bd62282939f6b69837ee53d21d31bdaa5
HeejeongRyu <95456557+HeejeongRyu@users.noreply.github.com>
2022-02-15 10:01:36 +0900

    2022-02-15
    복습

--- src/Silver/Silver_5_1037_1.java
+++ src/Silver/Silver_5_1037_1.java
@@ -1,4 +1,4 @@
-package Algorithm_2022_01_24;
+package Algorithm_2022_02_15;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,23 +10,21 @@ public class Silver_5_1037_1 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
 
-		int max = Integer.MIN_VALUE;
-		int min = Integer.MAX_VALUE;
-		
+		int [] arr = new int [n];
+
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		
-		for (int i = 0; i < n; i++) {
-			int k = Integer.parseInt(st.nextToken());
-			max = k > max ? k :max;
-			min = k < min ? k :min;
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
-		
-		System.out.println(max*min);
+
+		Arrays.sort(arr);
+
+		System.out.println(arr[0]*arr[n-1]);
 
 	}
 
