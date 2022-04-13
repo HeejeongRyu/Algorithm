87bf0f8fe01fe998648b1c53654676b73503fade
HeejeongRyu <95456557+HeejeongRyu@users.noreply.github.com>
2022-04-13 22:32:18 +0900

    Update Silver_5_2609_3.java

--- src/Silver/Silver_5_2609_3.java
+++ src/Silver/Silver_5_2609_3.java
@@ -1,10 +1,35 @@
 package Algorithm_2022_04_13;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Silver_5_2609_3 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+		int one = Integer.parseInt(st.nextToken());
+		int two = Integer.parseInt(st.nextToken());
+
+		int d = gcd(one, two);
+
+		System.out.println(d);
+		System.out.println(one * two / d);
+
+
+
+	}
+
+	static int gcd(int a, int b)	{
+
+		if(b==0)	return a;
+
+		return gcd(b, a%b);
 	}
 
 }
